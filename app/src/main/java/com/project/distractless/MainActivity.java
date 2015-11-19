afed53247ca11613cc5a57d32bc084143a3651d4
J.Novak <joseph.novak@gmail.com>
2015-11-19 11:19:28 -0500

    Added To-Do list skeleton

--- app/src/main/java/com/project/distractless/MainActivity.java
+++ app/src/main/java/com/project/distractless/MainActivity.java
@@ -1,5 +1,6 @@
 package com.project.distractless;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,22 +11,37 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
-    //String for password
-    public String key = "a";
-
+    //String for passwordFile
+    public static final String KEY = "KeyFile";
+    String keyInstance;
+    TextView keyView;
     TextView keyPrompt;
+    String keyEntry = "";
+    SharedPreferences keyStore;
+    Boolean noKey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView keyPrompt = (TextView) findViewById(R.id.keyPrompt);
-        //TODO Either code a Submit button or create a listener for when Key == Stored Key ...  then figure out how listeners work
-        //TODO Variate keyPrompt for if a key already exists in memory
-        //TODO Code memory... somehow
+
+        //Linking interface objects
+        keyPrompt = (TextView) findViewById(R.id.keyPrompt);
+        keyView = (TextView) findViewById(R.id.keyView);
+
+        keyStore = getSharedPreferences(KEY, 0);
+        //Get Stored Key
+        keyInstance = keyStore.getString("keyValue", "a");
+        if (keyInstance.equals("a")){
+            keyPrompt.setText("Select A 6 Digit Key");
+            noKey = true;
+        } else {
+            keyPrompt.setText("Enter Your Key");
+            noKey = false;
+        }
     }
 
-        //Code for NumberPad
+    //Code for NumberPad
     public void numPad (View view){
         switch(view.getId()){
             case R.id.numPad1:
@@ -61,12 +77,19 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    //Adds values to key
+    //Adds values to key, check password, if conditions are met spit output.
     public void numData (String num){
-        key = key+num;
-        if (key.length() >= 7){
-            keyPrompt = (TextView) findViewById(R.id.keyPrompt);
-            keyPrompt.setText("Great!");
+        keyEntry = keyEntry + num;
+        keyView.setText(keyEntry);
+        if (keyEntry.length() == 6 && noKey){
+            keyPrompt.setText("Saved!");
+            SharedPreferences.Editor keyEdit = keyStore.edit();
+            keyEdit.putString("keyValue", keyEntry);
+            keyEdit.commit();
+        } else if (keyEntry.equals(keyInstance)){
+            keyPrompt.setText("Match!");
+            //TODO Code Transition to next Activity once keys match.
         }
     }
+
 }
---
+++ app/src/main/java/com/project/distractless/ToDoList.java
@@ -0,0 +1,15 @@
+package com.project.distractless;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ToDoList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.to_do_list);
+        //TODO Code to do list according to guide at https://guides.codepath.com/android/Basic-Todo-App-Tutorial
+        //Layout file @ to_do_list.xml
+    }
+}
