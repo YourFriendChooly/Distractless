59b561df678b45d5e481cd1a31008cc564543cfe
WalterPN <kole.woodley@gmail.com>
2015-11-21 00:08:31 -0500

    I have everything added to the ToDoList.java file, but its not working. Android studio has a problem with the R in certain lines. Example: 29 ........(R.id.lvItems) 32 ........(R.Layout.to_do_list) 67 .......
    And a few more. It all works fine when I create an XML file here:
    
    ....../res/menu/main.xml
    
    However the code says it blanks and can't do anything with it. I've tried a number of things, and googled a bunch, but I'm not having any luck. Any way here it is.

--- app/src/main/java/com/project/distractless/MainActivity.java
+++ app/src/main/java/com/project/distractless/MainActivity.java
@@ -2,6 +2,7 @@ package com.project.distractless;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
@@ -9,10 +10,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/project/distractless/ToDoList.java
+++ app/src/main/java/com/project/distractless/ToDoList.java
@@ -1,15 +1,102 @@
 package com.project.distractless;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
-public class ToDoList extends AppCompatActivity {
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ToDoList extends AppCompatActivity
+{
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView lvItems;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.to_do_list);
         //TODO Code to do list according to guide at https://guides.codepath.com/android/Basic-Todo-App-Tutorial
-        //Layout file @ to_do_list.xml
+
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        items = new ArrayList<String>();
+        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        items.add("First Item");
+        items.add("Second Item");
+
+        readItems();
+        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+
+        setupListViewListener();
+    }
+
+    // Attaches a long click listener to the listview
+    private void setupListViewListener()
+    {
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener()
+        {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapter, View item, int pos, long id)
+            {
+                // Remove the item within array at position
+                items.remove(pos);
+                // Refresh the adapter
+                itemsAdapter.notifyDataSetChanged();
+                writeItems(); // <---- Add this line
+                // Return true consumes the long click event (marks it handled)
+                return true;
+            }
+
+        });
+    }
+
+    public void onAddItem(View v)
+    {
+        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
+        writeItems(); // <---- Add this line
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu); //Broken??
+        return true;
+    }
+    private void readItems()
+    {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            items = new ArrayList<String>(FileUtils.readLines(todoFile));
+        } catch (IOException e) {
+            items = new ArrayList<String>();
+        }
+    }
+
+    private void writeItems()
+    {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            FileUtils.writeLines(todoFile, items);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
