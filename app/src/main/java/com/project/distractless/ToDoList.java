ce9662b363288e4adb9c9de54d5fab7719b9a41f
YourFriendChooly <joe.budic@gmail.com>
2015-11-23 18:32:05 -0500

    Changed todo-list slightly, added a "next" button via a menu xml resource (may not be functioning properly at the moment.) Please code the if/else statements within the runCheck method (or similar, check the todo tab for details)

--- app/src/main/java/com/project/distractless/ToDoList.java
+++ app/src/main/java/com/project/distractless/ToDoList.java
@@ -1,8 +1,14 @@
 package com.project.distractless;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.InputType;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -19,11 +25,19 @@ public class ToDoList extends AppCompatActivity
     public static ArrayList<String> items;
     private ArrayAdapter<String> itemsAdapter;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.to_do_list);
+        /*
+        Create Toolbar
+         */
+        Toolbar toolbar = (Toolbar) findViewById(R.id.todo_toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("Create List");
+
         ListView lvItems = (ListView) findViewById(R.id.lvItems);
         readItems();
         items = new ArrayList<String>();
@@ -32,27 +46,59 @@ public class ToDoList extends AppCompatActivity
         lvItems.setAdapter(itemsAdapter);
 
         /*
-        Floating Action Button creates a dialogue to enter items to the to-do-list
+        Floating Action Button creates a dialogue to enter items to the to-do-list.
+        Within the FAB On Click Listener is the Alert Dialog pop-up
          */
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //TODO Code Dialog pop-up for text entry.
+                /*
+                Alert Dialog builder will prompt the user to enter text for the to-do list entry
+                and will proceed to allow the user to save or cancel the inputted text. That
+                information, if saved, will call the writeItems method to save the data to a text file.
+                 */
+                AlertDialog.Builder builder = new AlertDialog.Builder(ToDoList.this);
+                builder.setTitle("Create an Item");
+                final EditText input = new EditText(ToDoList.this);
+                input.setInputType(InputType.TYPE_CLASS_TEXT);
+                builder.setView(input);
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        itemsAdapter.add(input.getText().toString());
+                        itemsAdapter.notifyDataSetChanged();
+                        input.setText("");
+                        writeItems();
+                    }
+                });
+                builder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
             }
         });
     }
 
-    public void onAddItem(View v)
-    {
-        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
-        String itemText = etNewItem.getText().toString();
-        itemsAdapter.add(itemText);
-        itemsAdapter.notifyDataSetChanged();
-        etNewItem.setText("");
-        writeItems();
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.forward:
+                runCheck();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
+    public void runCheck(){
+        //TODO CODE THE INTENT IF/ELSE FOR IF ACTIVITY HAS BEEN RUN AND DIRECT FLOW.
+        //Alarm Fragment.class will be changed to the advanced settings menu, once it has been created.
+        startActivity(new Intent(ToDoList.this, alarmFragment.class));
+    }
 
     public void readItems()
     {
@@ -77,3 +123,5 @@ public class ToDoList extends AppCompatActivity
     }
 
 }
+
+
--- app/src/main/java/com/project/distractless/alarmFragment.java
+++ app/src/main/java/com/project/distractless/alarmFragment.java
@@ -59,7 +59,7 @@ public class alarmFragment extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the todo_toolbar; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_alarm, menu);
         return true;
     }
